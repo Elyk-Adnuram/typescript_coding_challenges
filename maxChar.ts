56346da3357fc4af5d6b6d6d91461392067865c9
Kyle Marunda <97623896+Elyk-Adnuram@users.noreply.github.com>
2024-07-05 10:04:35 +0200

    maxChar

--- maxChar.ts
+++ maxChar.ts
@@ -1,16 +1,15 @@
 function maxChar(str: string): {} {
-  //iterate through string
+  //create object to hold occurrences of each char in str
   let counter = {};
   for (let char of str) {
-    //if the char is in the counter then increment by 1
+    //if the char is in the counter then increment it by 1 in the counter obj
     if (counter[char]) {
       counter[char] += 1;
-      //if the char is not in the counter then set value to 1
+      //if the char is not in the counter then set value to 1 in the counter obj
     } else {
       counter[char] = 1;
     }
   }
-
   return counter;
 }
 //determine how many times each character occurs
