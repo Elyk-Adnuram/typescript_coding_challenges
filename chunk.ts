c14b5bee42efc3bd27bef3f5e75923f78bf5422e
Kyle Marunda <97623896+Elyk-Adnuram@users.noreply.github.com>
2024-07-10 19:25:16 +0200

    chunk

---
+++ chunk.ts
@@ -0,0 +1,33 @@
+//Given an array and chunk size, divide the array into many subarrays where each subarray is of length size
+
+function chunk(arr: number[], size: number): number[][] {
+  //Create an empty array to hold the chunks called chunked_arr
+  const chunked_arr = [];
+  //this is the start position for the slice method
+  let index = 0;
+  while (index < arr.length) {
+    //push sliced arrays into chunked_arr
+    chunked_arr.push(arr.slice(index, index + size));
+    index += size;
+  }
+  return chunked_arr;
+}
+
+// console.log(chunk([1, 2, 3, 4], 2));
+// console.log(chunk([1, 2, 3, 4, 5], 2));
+// console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
+
+function chunkArray<T>(array: T[], size: number): T[][] {
+  const chunks: T[][] = [];
+  //We use a for loop to iterate through the original array. The loop iterates with a step of size to ensure
+  //we move to the next chunk after adding elements to the current one.
+  for (let i = 0; i < array.length; i += size) {
+    const chunk = array.slice(i, i + size);
+    chunks.push(chunk);
+  }
+  return chunks;
+}
+
+console.log(chunkArray([1, 2, 3, 4], 2));
+console.log(chunkArray([1, 2, 3, 4, 5], 2));
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3));
